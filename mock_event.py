694b8f55adb665092c3335dd2b28fe166ba6639b
lenovo-0223-readme <2542368988>
2021-06-17 17:48:31 +0800

    mock实战作业 - 实现 MapLocal 修改雪球行情页的股票名称改为自己的名字

---
+++ mock_event.py
@@ -0,0 +1,22 @@
+import json
+import mitmproxy
+from mitmproxy import http
+
+
+# rewrite功能
+
+
+class AD:
+    def request(self, flow):
+        pass
+
+    def response(self, flow: mitmproxy.http.HTTPFlow):
+        if "https://stock.xueqiu.com/v5/stock/batch/quote.json?_t=" in flow.request.pretty_url:
+            data = json.loads(flow.response.text)
+            data["data"]["items"][0]["quote"]["name"] = "hogwarts"
+            flow.response.text = json.dumps(data)
+
+
+addons = [
+    AD()
+]
