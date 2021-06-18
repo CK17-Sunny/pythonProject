1e1c8482d3c197aa6ef5351b085d3e5608004e45
lenovo-0223-readme <2542368988>
2021-06-18 11:59:45 +0800

    mock 实战作业- 实现 Rewrite 实现股票颜色变换的的边界值测试

--- mock_event.py
+++ mock_event.py
@@ -13,7 +13,9 @@ class AD:
     def response(self, flow: mitmproxy.http.HTTPFlow):
         if "https://stock.xueqiu.com/v5/stock/batch/quote.json?_t=" in flow.request.pretty_url:
             data = json.loads(flow.response.text)
-            data["data"]["items"][0]["quote"]["name"] = "hogwarts"
+            data["data"]["items"][0]["quote"]["name"] = "WuSunny_rewrite"
+            data["data"]["items"][0]["quote"]["current"] = "50.12"
+            data["data"]["items"][0]["quote"]["percent"] = "-1.09"
             flow.response.text = json.dumps(data)
 
 
